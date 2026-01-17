68f4074d12a9787fb9db66a9e571f3d2e830eb07
Scott LaBounty <slabounty@Scotts-MacBook-Air.local>
2026-01-17 15:17:13 -0800

    Add Learning
    * Add learning mode
    * Add tests

---
+++ app/controllers/learnings_controller.rb
@@ -0,0 +1,22 @@
+# app/controllers/learnings_controller.rb
+class LearningsController < ApplicationController
+  def new
+    # just renders selection UI
+  end
+
+  def show
+    @mode = params[:mode] || "all_cards"
+    @reversed = ActiveModel::Type::Boolean.new.cast(params[:reversed])
+
+    cards = case @mode
+            when "major"
+              TarotCard.major_arcana
+            when "cups", "wands", "swords", "pentacles"
+              TarotCard.joins(:suit).where(suits: { name: @mode.capitalize })
+            else
+              TarotCard.all
+            end
+
+    @card = cards.order("RANDOM()").first
+  end
+end
--- app/models/tarot_card.rb
+++ app/models/tarot_card.rb
@@ -3,6 +3,10 @@ class TarotCard < ApplicationRecord
   has_many :users, through: :card_of_the_days
   belongs_to :suit
 
+  # app/models/tarot_card.rb
+  scope :major_arcana, -> { where(arcana: "Major") }
+  scope :minor_arcana, -> { where(arcana: "Minor") }
+
   def self.random_cards(count = 1)
     order("RANDOM()").limit(count)
   end
